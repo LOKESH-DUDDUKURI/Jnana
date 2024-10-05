309e4c8ad3c5928592cf4722b1106d537e8804f6
LOKESH DUDDUKURI <139634784+LOKESH-DUDDUKURI@users.noreply.github.com>
2024-10-05 16:45:33 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which protocol is used to send emails?",
+    answer: "SMTP",
+    options: [
+      "FTP",
+      "HTTP",
+      "SMTP",
+      "TCP"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which country won the FIFA World Cup in 2018?",
+    answer: "France",
+    options: [
+      "Brazil",
+      "Germany",
+      "France",
+      "Argentina"
+    ]
+  },
+  {
+    numb:3,
+    question: "A car travels 150 km in 3 hours. What is the speed of the car?",
+    answer: "50 km/h",
+    options: [
+      "30 km/h",
+      "50 km/h",
+      "60 km/h",
+      "45 km/h"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the primary purpose of data normalization in data science?",
+    answer: "To scale the data to a standard range.",
+    options: [
+      "To combine different data types.",
+      "To reduce the data size.",
+      "To scale the data to a standard range.",
+      "To eliminate duplicate records."
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which of the following options gives the correct syntax to create a view in SQL?",
+    answer: "CREATE VIEW view_name AS SELECT column1, column2 FROM table_name;",
+    options: [
+      "CREATE VIEW view_name SELECT column1, column2 FROM table_name;",
+      "CREATE VIEW view_name AS SELECT * FROM table_name;",
+      "CREATE VIEW view_name AS SELECT column1, column2 FROM table_name;",
+      "VIEW view_name AS SELECT column1, column2 FROM table_name;"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the synonym of the word 'elated'?",
+    answer: "Overjoyed",
+    options: [
+      "Angry",
+      "Overjoyed",
+      "Confused",
+      "Indifferent"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Find the next number in the sequence: 2, 4, 8, 16, ___?",
+    answer: "32",
+    options: [
+      "24",
+      "32",
+      "64",
+      "48"
+    ]
+  }
+];
